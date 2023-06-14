eef314c5688d2d47e5dcb020de77d391aecc9d2a
Amr M. Elsayed <rixtrayker@github.com>
2023-06-15 01:43:59 +0300

    Add eslinter and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
