c35b49f9044fc43714eb2dca1050314aaf682231
Amr M. Elsayed <rixtrayker@github.com>
2023-06-18 00:10:38 +0300

    Add Swagger Documentation

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const swagger = require('./utils/swagger');
 var indexRouter = require('./routes/index');
 var urlChecksRouter = require('./routes/urlChecks');
 
 var app = express();
 
+swagger(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
