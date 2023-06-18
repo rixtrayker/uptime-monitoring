8c38cda148670e666673beb39485cd7adb08bd59
Amr M. Elsayed <rixtrayker@github.com>
2023-06-18 13:22:52 +0300

    Add swagger docs

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,20 +1,7 @@
-const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
-const swaggerDocument = YAML.load('./swagger.yaml');
-
-const options = {
-  swaggerDefinition: {
-    info: {
-      title: 'Monitoring Service API Documentation',
-      version: '1.0.0',
-    },
-  },
-  apis: ['../routes/*.js>'],
-};
-
-const specs = swaggerJsDoc(options);
+const swaggerDocument = YAML.load(`${__dirname}/../swagger.yaml`);
 
 module.exports = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
